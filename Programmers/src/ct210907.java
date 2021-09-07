f6ce66e2c08cd71f3cb2ffd65020cd2ce0469de5
Nahyun Cho <w2038@e-mirim.hs.kr>
2021-09-07 10:09:26 +0900

    9월 7일 Commit

---
+++ Programmers/src/ct210907.java
@@ -0,0 +1,14 @@
+// 서울에서 김서방 찾기
+
+class Solution7 {
+    public String solution(String[] seoul) {
+        String answer = "김서방은 ";
+        for(int i = 0; i < seoul.length; i++){
+            if(seoul[i].equals("Kim")){
+                answer += i;
+            }
+        }
+        answer += "에 있다";
+        return answer;
+    }
+}
