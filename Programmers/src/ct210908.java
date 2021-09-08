5216cc99750a14920ab9594c477f69f0311718f8
Nahyun Cho <w2038@e-mirim.hs.kr>
2021-09-08 13:02:46 +0900

    9월 8일 Commit

---
+++ Programmers/src/ct210908.java
@@ -0,0 +1,15 @@
+// 문자열 다루기 기본
+
+class Solution {
+    public boolean solution(String s) {
+        try{
+            int s1 = Integer.parseInt(s);
+            if(s.length() == 4 || s.length() == 6){
+                return true;
+            }
+            return false;
+        }catch(NumberFormatException e){
+            return false;
+        }
+    }
+}
