2f1b939c2ba2d0e102b9f84fbe8b60e3c81210b2
Nahyun Cho <w2038@e-mirim.hs.kr>
2021-09-09 10:57:36 +0900

    9월 9일 Commit

---
+++ Programmers/src/ct210909.java
@@ -0,0 +1,18 @@
+// 문자열 내림차순으로 배치하기
+
+import java.util.Arrays;
+
+class Solution9 {
+    public String solution(String s) {
+        String answer = "";
+        String arr[] = new String[s.length()];
+        for(int i = 0; i < s.length(); i++){
+            arr[i] = String.valueOf(s.charAt(i));
+        }
+        Arrays.sort(arr);
+        for(int i = s.length() - 1; i >= 0; i--){
+            answer += arr[i];
+        }
+        return answer;
+    }
+}
