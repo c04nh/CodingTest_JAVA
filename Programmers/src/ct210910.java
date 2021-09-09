fc1fe4f60235c8b9e10fdf26fe5deb5cac502c2d
Nahyun Cho <w2038@e-mirim.hs.kr>
2021-09-10 08:44:24 +0900

    9월 10일 Commit

---
+++ Programmers/src/ct210910.java
@@ -0,0 +1,22 @@
+// 문자열 내 p와 y의 개수
+
+class Solution10 {
+    boolean solution(String s) {
+        boolean answer = true;
+        int countP = 0;
+        int countY = 0;
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == 'P' || s.charAt(i) == 'p'){
+                countP++;
+            }else if(s.charAt(i) == 'Y' || s.charAt(i) == 'y'){
+                countY++;
+            }
+        }
+        if(countP == countY){
+            answer = true;
+        }else{
+            answer = false;
+        }
+        return answer;
+    }
+}
