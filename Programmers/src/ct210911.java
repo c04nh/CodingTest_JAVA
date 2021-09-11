1a7f991c4c08efd5f2f2f175fbfa275a6a509d0d
Nahyun Cho <w2038@e-mirim.hs.kr>
2021-09-11 11:46:26 +0900

    9월 11일 Commit

---
+++ Programmers/src/ct210911.java
@@ -0,0 +1,19 @@
+// 두 정수 사이의 합
+
+class Solution11 {
+    public long solution(int a, int b) {
+        long answer = 0;
+        if(a < b){
+            for(int i = a; i <= b; i++){
+                answer += i;
+            }
+        }else if(a == b){
+            answer = a;
+        }else{
+            for(int i = b; i <= a; i++){
+                answer += i;
+            }
+        }
+        return answer;
+    }
+}
