e07a3f3f59e5e1f27e9684e6f4f64f348fdca73e
Nahyun Cho <w2038@e-mirim.hs.kr>
2021-09-19 00:39:06 +0900

    9월 19일 Commit

---
+++ Programmers/src/ct210919.java
@@ -0,0 +1,11 @@
+// 내적
+
+class Solution19 {
+    public int solution(int[] a, int[] b) {
+        int answer = 0;
+        for(int i = 0; i < a.length; i++){
+            answer += a[i] * b[i];
+        }
+        return answer;
+    }
+}
