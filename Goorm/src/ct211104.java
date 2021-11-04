7e2dfa21c80f9c53a2025c229953ff40686776b3
c04nh <w2038@e-mirim.hs.kr>
2021-11-04 13:29:16 +0900

    11월 4일 Commit

---
+++ Goorm/src/ct211104.java
@@ -0,0 +1,13 @@
+// 1단계
+// Hello Goorm !
+
+import java.io.*;
+
+class Solution211104 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String input = br.readLine();
+		int num = Integer.parseInt(input);
+		for(int i = 0; i < num; i++) System.out.println("Hello Goorm !");
+	}
+}
