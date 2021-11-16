d8ae7e3f715ff79cd5b1b15c473764b0d335e89d
c04nh <w2038@e-mirim.hs.kr>
2021-11-17 00:43:13 +0900

    11월 17일 Commit

---
+++ Goorm/src/ct211117.java
@@ -0,0 +1,13 @@
+// 1단계
+// ASCII 코드
+
+import java.io.*;
+class Solution211117 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String input = br.readLine();
+		char ch = input.charAt(0);
+		int num = (int)ch;
+		System.out.println(num);
+	}
+}
