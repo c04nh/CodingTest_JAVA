fff45f748a7d31ba471e2c3d904908e5a0463e5e
c04nh <w2038@e-mirim.hs.kr>
2021-12-08 14:33:53 +0900

    12월 8일 Commit

---
+++ Baekjoon/src/ct211208.java
@@ -0,0 +1,10 @@
+// 고양이
+
+class Solution211208{
+    public static void main(String[] args){
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( \')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
