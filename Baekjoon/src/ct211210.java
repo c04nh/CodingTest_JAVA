2b4c18e7695f8bb14e3865b6598bb005a84f3a14
c04nh <w2038@e-mirim.hs.kr>
2021-12-10 13:40:54 +0900

    12월 10일 Commit

---
+++ Baekjoon/src/ct211210.java
@@ -0,0 +1,12 @@
+// A+B
+
+import java.util.Scanner;
+
+class Solution211210{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int num1 = scan.nextInt();
+        int num2 = scan.nextInt();
+        System.out.print(num1 + num2);
+    }
+}
