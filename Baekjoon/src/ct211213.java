8a2ffb23028b6a6b970adf187adfd78d3d7a808a
c04nh <w2038@e-mirim.hs.kr>
2021-12-13 14:53:26 +0900

    12월 13일 Commit

---
+++ Baekjoon/src/ct211213.java
@@ -0,0 +1,13 @@
+// A/B
+
+import java.util.Scanner;
+
+class Solution211213{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        double num1 = scan.nextDouble();
+        double num2 = scan.nextDouble();
+        double result = num1 / num2;
+        System.out.print(result);
+    }
+}
