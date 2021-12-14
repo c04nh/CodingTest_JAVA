e1a449b9e90887452b2fb684dc3a6d34790f9a29
c04nh <w2038@e-mirim.hs.kr>
2021-12-14 21:37:23 +0900

    12월 14일 Commit

---
+++ Baekjoon/src/ct211214.java
@@ -0,0 +1,16 @@
+// 사칙연산
+
+import java.util.Scanner;
+
+class Solution211214{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int num1 = scan.nextInt();
+        int num2 = scan.nextInt();
+        System.out.println(num1 + num2);
+        System.out.println(num1 - num2);
+        System.out.println(num1 * num2);
+        System.out.println(num1 / num2);
+        System.out.println(num1 % num2);
+    }
+}
