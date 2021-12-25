99c7096b4d4c18395089d2bab2c1b51dc83bd82e
c04nh <w2038@e-mirim.hs.kr>
2021-12-26 00:24:19 +0900

    12월 26일 Commit

---
+++ Baekjoon/src/ct211226.java
@@ -0,0 +1,13 @@
+// N 찍기
+
+import java.util.Scanner;
+
+class Solution211226{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+        for(int i = 1; i <= num; i++){
+            System.out.println(i);
+        }
+    }
+}
