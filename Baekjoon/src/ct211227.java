faff0e2d0135d853c2fb313e28f637ee3eac0c50
c04nh <w2038@e-mirim.hs.kr>
2021-12-27 11:18:47 +0900

    12월 27일 Commit

---
+++ Baekjoon/src/ct211227.java
@@ -0,0 +1,13 @@
+// 기찍 N
+
+import java.util.Scanner;
+
+class Solution211227{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+        for(int i = num; i >= 1; i--){
+            System.out.println(i);
+        }
+    }
+}
