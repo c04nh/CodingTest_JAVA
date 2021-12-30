775519c85537dc00f98dd71c7a0261647edf8cdc
c04nh <w2038@e-mirim.hs.kr>
2021-12-30 11:22:04 +0900

    12월 30일 Commit

---
+++ Baekjoon/src/ct211230.java
@@ -0,0 +1,16 @@
+// 별 찍기 - 1
+
+import java.util.Scanner;
+
+class Solution211230{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+        for(int i = 1; i <= num; i++){
+            for(int j = 1; j <= i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
