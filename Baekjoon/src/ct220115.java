f4a0e3a31f7831853cf15317d75aaddaa1bc7f19
c04nh <w2038@e-mirim.hs.kr>
2022-01-15 12:27:24 +0900

    1월 15일 Commit

---
+++ Baekjoon/src/ct220115.java
@@ -0,0 +1,11 @@
+// 아스키 코드
+
+import java.util.Scanner;
+
+class Solution220115{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int ch = scan.next().charAt(0);
+        System.out.print(ch);
+    }
+}
