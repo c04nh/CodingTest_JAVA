d5a41748f2f85cb7f4e8bcff93210ad3ff697a5c
c04nh <w2038@e-mirim.hs.kr>
2022-01-29 00:04:32 +0900

    1월 29일 Commit

---
+++ Baekjoon/src/ct220129.java
@@ -0,0 +1,11 @@
+// ??!
+
+import java.util.Scanner;
+
+class Solution220129{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        String id = scan.next();
+        System.out.print(id + "??!");
+    }
+}
