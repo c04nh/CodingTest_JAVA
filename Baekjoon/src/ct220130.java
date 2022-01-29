5ec4231f7f1a7da514d06511b3e281d79a41ecc7
c04nh <w2038@e-mirim.hs.kr>
2022-01-30 00:39:54 +0900

    1월 30일 Commit

---
+++ Baekjoon/src/ct220130.java
@@ -0,0 +1,11 @@
+// 1998년생인 내가 태국에서는 2541년생?!
+
+import java.util.Scanner;
+
+class Solution220130{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int year = scan.nextInt();
+        System.out.print(year - 543);
+    }
+}
