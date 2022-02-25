4a1a296f1231c884ab03ff27a369b20b42bd7fdc
c04nh <w2038@e-mirim.hs.kr>
2022-02-26 00:54:47 +0900

    2월 26일 Commit

---
+++ Baekjoon/src/ct220226.java
@@ -0,0 +1,19 @@
+// 영화감독 숌
+
+import java.util.Scanner;
+ 
+class Solution220226{
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int N = scan.nextInt();
+		int num = 666;
+		int count = 1;
+		while(count != N) {
+			num++;
+			if(String.valueOf(num).contains("666")) {
+				count++;
+			}
+		}
+		System.out.println(num);
+	}
+}
