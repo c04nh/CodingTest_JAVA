d5c16b4de34107ac0d82d13b8aee6bac7bc77843
c04nh <w2038@e-mirim.hs.kr>
2022-03-13 21:46:35 +0900

    3월 13일 Commit

---
+++ Baekjoon/src/ct220313.java
@@ -0,0 +1,44 @@
+// N-Queen
+
+import java.util.Scanner;
+
+class Solution220313{
+	public static int[] arr;
+	public static int N;
+	public static int count = 0;
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		N = scan.nextInt();
+		arr = new int[N];
+		nQueen(0);
+		System.out.println(count);
+
+	}
+
+	public static void nQueen(int depth) {
+		if (depth == N) {
+			count++;
+			return;
+		}
+
+		for (int i = 0; i < N; i++) {
+			arr[depth] = i;
+			if (Possibility(depth)) {
+				nQueen(depth + 1);
+			}
+		}
+
+	}
+
+	public static boolean Possibility(int col) {
+		for (int i = 0; i < col; i++) {
+			if (arr[col] == arr[i]) {
+				return false;
+			}
+			else if (Math.abs(col - i) == Math.abs(arr[col] - arr[i])) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
