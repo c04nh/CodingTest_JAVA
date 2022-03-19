4a0c1b3b3f9cc7e6f3b0f09e5f6082bf0209a6fc
c04nh <w2038@e-mirim.hs.kr>
2022-03-19 11:20:49 +0900

    3월 19일 Commit

---
+++ Baekjoon/src/ct220319.java
@@ -0,0 +1,25 @@
+// 01타일
+
+import java.util.Scanner;
+
+class Solution220319{
+    public static int[] dp = new int[1000001];;
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt();
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+        for(int i = 3; i < dp.length; i++) {
+            dp[i] = -1;
+        }
+        System.out.println(Tile(N));
+    }
+
+    public static int Tile(int N) {
+        if(dp[N] == -1) {
+            dp[N] = (Tile(N - 1) + Tile((N - 2))) % 15746;
+        }
+        return dp[N];
+    }
+}
