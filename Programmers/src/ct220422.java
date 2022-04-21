fcb89225b1eec85a1d66962128f4546c8eb10c7f
nhsally <nhsally@naver.com>
2022-04-22 08:55:34 +0900

    4월 22일 Commit

---
+++ Programmers/src/ct220422.java
@@ -0,0 +1,15 @@
+// 2단계
+// 전화번호 목록
+
+import java.util.Arrays;
+
+class Solution220422 {
+    public boolean solution(String[] phone_book) {
+        boolean answer = true;
+        Arrays.sort(phone_book);
+        for (int i = 0; i < phone_book.length - 1; i++){
+            if (phone_book[i + 1].startsWith(phone_book[i])) return false;
+        }
+        return answer;
+    }
+}
