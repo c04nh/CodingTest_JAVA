529f4d653916e58e22c12f9720a1c83fe3714f59
c04nh <w2038@e-mirim.hs.kr>
2022-06-05 22:39:42 +0900

    6월 5일 Commit

---
+++ Baekjoon/src/ct220605.java
@@ -0,0 +1,36 @@
+// 나무 자르기
+
+import java.util.Scanner;
+
+class Solution220605 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt();
+        int M = scan.nextInt();
+        int[] tree = new int[N];
+        int min = 0;
+        int max = 0;
+        for(int i = 0; i < N; i++) {
+            tree[i] = scan.nextInt();
+            if(max < tree[i]) {
+                max = tree[i];
+            }
+        }
+
+        while(min < max) {
+            int mid = (min + max) / 2;
+            long sum = 0;
+            for(int treeHeight : tree) {
+                if(treeHeight - mid > 0) {
+                    sum += (treeHeight - mid);
+                }
+            }
+            if(sum < M) {
+                max = mid;
+            }else {
+                min = mid + 1;
+            }
+        }
+        System.out.println(min - 1);
+    }
+}
