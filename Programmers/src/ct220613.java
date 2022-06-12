84ba8c9bbc0bf5c5eb461fcfeab30661b3af0852
nhsally <nhsally@naver.com>
2022-06-13 08:37:13 +0900

    6월 13일 Commit

---
+++ Programmers/src/ct220613.java
@@ -0,0 +1,28 @@
+// 2단계
+// 가장 큰 수
+
+import java.util.*;
+
+class Solution220613{
+    public String solution(int[] numbers){
+        String answer = "";
+        String[] str = new String[numbers.length];
+
+        for(int i = 0; i < numbers.length; i++){
+            str[i] = String.valueOf(numbers[i]);
+        }
+
+        Arrays.sort(str, new Comparator<String>(){
+            @Override
+            public int compare(String a, String b){
+                return (b + a).compareTo(a + b);
+            }
+        });
+
+        if(str[0].equals("0")) return "0";
+
+        for(String s: str) answer += s;
+
+        return answer;
+    }
+}
