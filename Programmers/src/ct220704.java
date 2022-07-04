6f92e51f5a5d6ac6852ebe3447764fec107155d6
nhsally <nhsally@naver.com>
2022-07-04 13:59:44 +0900

    7월 4일 Commit

---
+++ Programmers/src/ct220704.java
@@ -0,0 +1,16 @@
+// 2단계
+// 주식가격
+
+class Solution220704 {
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+        for (int i = 0; i < prices.length; i++) {
+            for (int j = i + 1; j < prices.length; j++) {
+                answer[i]++;
+                if (prices[i] > prices[j])
+                    break;
+            }
+        }
+        return answer;
+    }
+}
