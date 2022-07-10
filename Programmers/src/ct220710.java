119126bb8236e6909d6d07d181870f38f8c8c55c
c04nh <w2038@e-mirim.hs.kr>
2022-07-10 22:17:32 +0900

    7월 10일 Commit

---
+++ Programmers/src/ct220710.java
@@ -0,0 +1,24 @@
+// 2단계
+// 구명보트
+
+import java.util.*;
+
+class Solution220710 {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        Arrays.sort(people);
+        int index = 0;
+
+        for (int i = people.length - 1; i >= index; i--) {
+            if (people[i] + people[index] <= limit) {
+                index++;
+                answer++;
+            }
+            else {
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+}
